ed57307b31ce50ae4c78b0fdc4c02ea0d0e42b85
Duong <phduongit2k1@gmail.com>
2021-11-13 11:06:30 +0700

    Fix .env and server.js

--- back-end/server.js
+++ back-end/server.js
@@ -1,30 +1,33 @@
+// Environment constants
 require('dotenv').config();
 
+// Third-party packages
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-const app = express()
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
-
-const CONNECTION_URI = 'mongodb://localhost:27017/multichoice';
-const PORT = process.env.port || 5000;
-
-mongoose.connect(CONNECTION_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
-
-app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}!!`);
-});
-
+// Local
 const loginApi = require('./src/apis/login.api');
 const examApi = require('./src/apis/exam.api');
 
-// API
+// Configurations
+const app = express()
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+// Mongodb connection via Mongoose
+mongoose.connect(process.env.MONGO_CONNECTION_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
+
+// Listening
+app.listen(process.env.PORT, () => {
+	console.log(`Server is listening on port ${process.env.PORT}!!`);
+});
+
+// APIs
 app.use('/login', loginApi);
 app.use('/exam', examApi);
