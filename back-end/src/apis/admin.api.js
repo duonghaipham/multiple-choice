8bd272d81da6b7cf234e2dfd7bf3cd07720578c9
hdDuy0505 <92098917+hdDuy0505@users.noreply.github.com>
2022-01-02 20:08:08 +0700

    Add auth middleware

--- back-end/src/apis/admin.api.js
+++ back-end/src/apis/admin.api.js
@@ -1,23 +1,40 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const adminController = require('../controllers/admin.controller');
+const adminController = require("../controllers/admin.controller");
+const adminMiddleware = require("../middlewares/admin.middleware");
 
-router.post('/exams/create', adminController.postCreateExam);
+router.post("/exams/create", adminController.postCreateExam);
 
-router.get('/exams', adminController.getRetrieveExams);
+router.get("/exams", adminController.getRetrieveExams);
 
-router.get('/exams/:id/update', adminController.getUpdateExam);
+router.get("/exams/:id/update", adminController.getUpdateExam);
 
-router.put('/exams/:id/update', adminController.putUpdateExam);
+router.put("/exams/:id/update", adminController.putUpdateExam);
 
-router.delete('/exams/:id/delete', adminController.deleteExam);
+router.delete("/exams/:id/delete", adminController.deleteExam);
 
-router.post('/users/create', adminController.postCreateUser);
+router.post(
+  "/users/create",
+  adminMiddleware.adminMiddleware,
+  adminController.postCreateUser
+);
 
-router.get('/users', adminController.getRetrieveUsers);
+router.get(
+  "/users",
+  adminMiddleware.adminMiddleware,
+  adminController.getRetrieveUsers
+);
 
-router.put('/users/:id/update', adminController.putUpdateUser);
+router.put(
+  "/users/:id/update",
+  adminMiddleware.adminMiddleware,
+  adminController.putUpdateUser
+);
 
-router.delete('/users/:id/delete', adminController.deleteUser);
+router.delete(
+  "/users/:id/delete",
+  adminMiddleware.adminMiddleware,
+  adminController.deleteUser
+);
 
 module.exports = router;
---
+++ back-end/src/middlewares/admin.middleware.js
@@ -0,0 +1,27 @@
+const userModel = require("../models/user.model");
+const jwt = require('jsonwebtoken');
+const adminMiddleware = async (req, res, next) => {
+  //giải mã token lấy userId
+  if (req.headers.access_token) {
+    const decodedAuth = await jwt.verify(
+      req.headers.access_token,
+      process.env.JWT_SECRET_KEY
+    );
+    const { userId } = decodedAuth;
+
+    const user = await userModel.findById(userId);
+    if (user.role === "admin") {
+      next();
+    } else {
+      res.status(400).json({
+        message: "Unauthorized",
+      });
+    }
+  } else {
+    res.status(400).json({
+      message: "Unauthorized",
+    });
+  }
+};
+
+module.exports = { adminMiddleware };
